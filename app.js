1dd7c6b4ad56c40c5460a26868a80beeb9d7b168
Joe-Nava <joelsaid.navarios@gmail.com>
2025-03-17 23:54:17 -0600

    Proyecto final Amigo Secreto
    Lógica de programación aplicada a los puntos principales del desafío

---
+++ app.js
@@ -0,0 +1,21 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos")
+const ulResultado = document.getElementById("resultado")
+
+function agregarAmigo() {
+    if (inputAmigo.value == ""){
+        alert("Aún no has colocado un nombre de amigo");
+        return;
+    };
+    
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+}
+
+function sortearAmigo(){
+    const numAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSecreto = listaAmigos[numAleatorio]
+    ulResultado.innerHTML = `<li>El amigo secreto sorteado fue: ${amigoSecreto}</li>`    
+};
